dff47e3e8386e3244c20fdbe185ab7497d4ea620
unknown <eskicmihajlo55@gmail.com>
2024-02-21 23:23:30 +0100

    added median func

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 
 const getMean = (array) =>array.reduce((acc, el)=> acc + el,0)/array.length;
 
+const getMedian = (array) => {
 
+    const sorted = array.sort((a, b) => a - b);
+    const median =
+      array.length % 2 === 0
+        ? getMean([sorted[array.length / 2], sorted[array.length / 2 - 1]])
+        : sorted[Math.floor(array.length / 2)];
+    return median;
+}
 
 
 
@@ -16,10 +24,10 @@ const numbers = array.map((num)=> Number(num)).filter((el)=>!isNaN(el)); // onve
 const mean = getMean(numbers);
 document.querySelector("#mean").textContent = mean;
 
-
+const  median = getMedian(numbers);
+document.querySelector("#median").textContent = median;
 
 }
-
 
 
 
